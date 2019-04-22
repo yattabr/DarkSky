208301cb26cf5cba0bcdc468502809cb638a7bbb
Eduardo Ewerton <edussj3@gmail.com>
2019-04-22 14:13:15 +0100

    Created Unit Tests and Espresso Tests

--- app/src/androidTest/java/br/com/wobbu/darksky/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/br/com/wobbu/darksky/ExampleInstrumentedTest.kt
@@ -1,24 +1,73 @@
 package br.com.wobbu.darksky
 
-import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-
+import br.com.wobbu.darksky.activities.MainActivity
+import br.com.wobbu.darksky.model.Currently
+import br.com.wobbu.darksky.model.Daily
+import br.com.wobbu.darksky.model.ForecastResponse
+import br.com.wobbu.darksky.model.Hourly
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    var activityTestRule = ActivityTestRule(MainActivity::class.java, true, true)
+    private lateinit var forecast: ForecastResponse
+
+    @Before
+    fun setup() {
+        forecast = ForecastResponse(Currently("", "73"), Hourly(""), Daily(""))
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("br.com.wobbu.darksky", appContext.packageName)
+    fun convertCelsiusToFahrenheit() {
+        activityTestRule.activity.mainViewModel.forecastObserver.postValue(forecast)
+
+        Thread.sleep(500)
+        Espresso.onView(allOf(withId(R.id.text_temperature))).perform(click())
+
+        fahrenheit("73")
+    }
+
+    @Test
+    fun convertFahrenheitToCelsius() {
+        activityTestRule.activity.mainViewModel.forecastObserver.postValue(forecast)
+        celsius("23")
+
+        Thread.sleep(500)
+        Espresso.onView(allOf(withId(R.id.text_temperature))).perform(click())
+
+        fahrenheit("73")
+        Thread.sleep(500)
+        Espresso.onView(allOf(withId(R.id.text_temperature))).perform(click())
+
+        celsius("23")
+    }
+
+    private fun celsius(value: String) {
+        Espresso.onView(allOf(withId(R.id.text_temperature), withText(value)))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        Espresso.onView(allOf(withId(R.id.text_temperature_type), withText("˚C")))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
+    }
+
+    private fun fahrenheit(value: String) {
+        Espresso.onView(allOf(withId(R.id.text_temperature), withText(value)))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        Espresso.onView(allOf(withId(R.id.text_temperature_type), withText("˚F")))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
     }
 }
--- app/src/test/java/br/com/wobbu/darksky/ExampleUnitTest.kt
+++ app/src/test/java/br/com/wobbu/darksky/ExampleUnitTest.kt
@@ -1,17 +1,31 @@
 package br.com.wobbu.darksky
 
+import br.com.wobbu.darksky.viewModel.MainViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+@RunWith(JUnit4::class)
 class ExampleUnitTest {
+
+    lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setup() {
+        viewModel = MainViewModel()
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun celsiusToFahrenheit_isCorrect() {
+        val temperature = viewModel.convertCelciusToFahrenheit("23")
+        assertEquals(temperature, "73")
+    }
+
+    @Test
+    fun fahrenheitToCelsius_isCorrect() {
+        val temperature = viewModel.convertFahrenheitToCelcius("73")
+        assertEquals(temperature, "23")
     }
 }
